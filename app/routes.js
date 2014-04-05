7a199af194a9160c6af46d2b6c565eb2a4ac3a64
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-04-05 11:14:37 -0400

    Changed the location of the success redirect

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ module.exports = function(app, passport) {
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/profile', // redirect to the secure profile section
+		successRedirect : 'http://purpl3lue.herokuapp.com/', // redirect to the secure profile section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
