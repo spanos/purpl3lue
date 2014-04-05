b984c0acfd283086414e83825b28603861eab7ea
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-04-05 10:55:42 -0400

    This includes login information as well as an edit to package.json

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://valakuzhy:password123@oceanic.mongohq.com:10008/app23761232' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
