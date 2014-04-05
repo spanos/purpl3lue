7d18de130e9fdba8eec5214b09f01e9aa97b36fd
dspan <david.spanos404@gmail.com>
2014-04-05 03:55:44 -0400

    changed server from localhost to herokuapp

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(req, res) {
 res.writeHead(200, { 'Content-type': 'text/html'});
 res.end(fs.readFileSync(__dirname + '/index.html'));
 }).listen(8080, function() {
-console.log('Listening at: http://localhost:8080');
+console.log('Listening at: http://purpl3lue.herokuapp.com');
 });
  
 socketio.listen(server).on('connection', function (socket) {
