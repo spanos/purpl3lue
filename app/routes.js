9a66cd797ac104de732a26c5ac5f9afd1e378a5e
Kevin Valakuzhy <kevin.valakuzhy@gmail.com>
2014-04-05 13:09:13 -0400

    Trying to show how this works with a different procfile

--- app/routes.js
+++ app/routes.js
@@ -20,7 +20,7 @@ module.exports = function(app, passport) {
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : 'http://purpl3lue.herokuapp.com/index.html', // redirect to the secure profile section
+		successRedirect : 'http://purpl3lue.herokuapp.com/', // redirect to the secure profile section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
