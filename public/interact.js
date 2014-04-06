4bb55beaafa11a0de08fd9f4e853e3a76e55cc93
dspan <david.spanos404@gmail.com>
2014-04-06 05:09:08 -0400

    added some jquery

--- public/interact.js
+++ public/interact.js
@@ -1,14 +1,10 @@
-$("li").click(function(){
-	var input = $('<input type="test" />',{
-		value: $(this).text()
-	}).click(function(){
-		$.get('editsave.php?tekstny=' + $(this).value(), function(response){
-			var span = $('span',{
-				text: response
-			});
-			$(this).replaceWith(span);
-		});
+$(document).ready(function(){
+
+	alert("Hello World");
+
+	$("li").hover(function(){
+		$( this ).addClass( "hover" );
+	}, function() {
+		$( this ).removeClass( "hover" );
 	});
-	$(this).replaceWith(input);
 });
-$("li").css("color","red");
